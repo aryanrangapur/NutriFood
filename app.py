a0ceea4f0f24d30e108101e87f65b6fefa102a91
aryanrangapur <aryan.rangapur717@gmail.com>
2025-10-06 11:59:28 +0530

    Use dynamic PORT for Railway deployment

--- app.py
+++ app.py
@@ -485,7 +485,9 @@ def get_fallback_nutrition(food_item, quantity):
         }
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
 else:
     # This is for Vercel serverless
     application = app
